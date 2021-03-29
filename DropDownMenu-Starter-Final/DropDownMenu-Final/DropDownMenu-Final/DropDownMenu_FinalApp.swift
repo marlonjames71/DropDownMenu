4ebba2294343b870b838baa51ba5edf9a0d45feb
Marlon Raskin <marlonr@hey.com>
2021-03-29 00:46:21 -0400

    Initial commit for final project

---
+++ DropDownMenu-Starter-Final/DropDownMenu-Final/DropDownMenu-Final/DropDownMenu_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownMenu_FinalApp.swift
+//  DropDownMenu-Final
+//
+//  Created by Marlon Raskin on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct DropDownMenu_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
