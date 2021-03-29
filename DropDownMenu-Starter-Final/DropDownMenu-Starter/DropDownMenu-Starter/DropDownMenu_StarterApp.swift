6550bd257a98b01f654484317b4bf1ebd1bd8291
Marlon Raskin <marlonr@hey.com>
2021-03-29 00:45:25 -0400

    Initial commit for starter project

---
+++ DropDownMenu-Starter-Final/DropDownMenu-Starter/DropDownMenu-Starter/DropDownMenu_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownMenu_StarterApp.swift
+//  DropDownMenu-Starter
+//
+//  Created by Marlon Raskin on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct DropDownMenu_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
