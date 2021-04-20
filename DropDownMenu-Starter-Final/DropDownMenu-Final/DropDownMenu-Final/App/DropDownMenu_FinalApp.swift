25be0e4ef3700ca58c7969d24396780062065f9c
Marlon Raskin <marlonr@hey.com>
2021-04-19 23:10:30 -0400

    Setup navigation styling

--- DropDownMenu-Starter-Final/DropDownMenu-Final/DropDownMenu-Final/App/DropDownMenu_FinalApp.swift
+++ DropDownMenu-Starter-Final/DropDownMenu-Final/DropDownMenu-Final/App/DropDownMenu_FinalApp.swift
@@ -9,9 +9,31 @@ import SwiftUI
 
 @main
 struct DropDownMenu_FinalApp: App {
+
+    let appearance = UINavigationBarAppearance()
+
+    // MARK: - Init
+
+    init() {
+        setNavBarAppearance()
+    }
+
+
+    // MARK: - Body
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+
+    // MARK: - Setup
+
+    func setNavBarAppearance() {
+        appearance.configureWithTransparentBackground()
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().tintColor = UIColor(.primary)
+        appearance.backgroundColor = UIColor(.bg).withAlphaComponent(0.95)
     }
 }
