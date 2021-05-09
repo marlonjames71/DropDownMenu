a5f0b677cbe5371b86fd340f7b8311dc1c67e49b
Marlon Raskin <marlonr@hey.com>
2021-05-09 13:24:27 -0400

    Start proj updates - color, data, proj org

--- DropDownMenu-Starter-Final/DropDownMenu-Starter/DropDownMenu-Starter/Helpers/Color+Ext.swift
+++ DropDownMenu-Starter-Final/DropDownMenu-Starter/DropDownMenu-Starter/Helpers/Color+Ext.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 extension Color {
-    static let blue = Color("blue")
-    static let green = Color("green")
-    static let greyText = Color("greyText")
     static let bg = Color("mainBG")
-    static let pink = Color("pink")
     static let secondaryBG = Color("secondaryBG")
+    static let greyText = Color("greyText")
+    static let ddmBlue = Color("blue")
+    static let ddmGreen = Color("green")
+    static let ddmPink = Color("pink")
 }
--- DropDownMenu-Starter-Final/DropDownMenu-Starter/DropDownMenu-Starter/Screens/ContentView.swift
+++ DropDownMenu-Starter-Final/DropDownMenu-Starter/DropDownMenu-Starter/Screens/ContentView.swift
@@ -8,6 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+
+//    let hobbyCategories: [Option] = [
+//        Option(title: "Listening to Music Listening to Music Listening to Music", iconName: "headphones"),
+//        Option(title: "Going to the Movies", iconName: "ticket.fill"),
+//        Option(title: "Playing Games", iconName: "gamecontroller.fill"),
+//        Option(title: "Stand-up Comedy", iconName: "music.mic"),
+//        Option(title: "Photography", iconName: "camera.fill"),
+//        Option(title: "Reading", iconName: "books.vertical.fill"),
+//        Option(title: "Cycling", iconName: "bicycle"),
+//        Option(title: "Traveling", iconName: "tram.fill"),
+//        Option(title: "Investing", iconName: "bitcoinsign.square.fill"),
+//    ]
+
+//        let hobbyCategories: [Option] = [
+//            Option(title: "Listening to Music"),
+//            Option(title: "Going to the Movies"),
+//            Option(title: "Playing Games"),
+//            Option(title: "Stand-up Comedy"),
+//            Option(title: "Photography"),
+//            Option(title: "Reading"),
+//            Option(title: "Cycling"),
+//            Option(title: "Traveling"),
+//            Option(title: "Investing"),
+//        ]
+
     var body: some View {
         Text("Hello, world!")
             .padding()
